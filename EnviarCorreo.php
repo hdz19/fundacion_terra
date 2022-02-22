f588a65325f3329061cc7896d4fd23f39db5adb9
Michell <Michellochoa91@gmail.com>
2022-02-22 17:44:05 -0600

    Update Michell (5:43 pm)

--- EnviarCorreo.php
+++ EnviarCorreo.php
@@ -1,4 +1,11 @@
 <?php
-    mail("michellochoa997@gmail.com", "subject", "body", "From: michellochoa99@gmail.com");
+    session_start();
+    $usuario=$_SESSION['Usuario'];
+    mail("michellochoa997@gmail.com", "Contraseña reestablecida", "Querido (a) $usuario ,
+     Hemos reestablecido tu contraseña correctamente.
+     Para volver a ingresar utiliza la siguiente contraseña:  ",
+     "From: michellochoa99@gmail.com");
+    
  ?>
+ 
  
